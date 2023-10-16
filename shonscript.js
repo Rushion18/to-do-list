60242e329121575b6924043efb09c04fc778efa2
Rushion18! <francisgchegge@gmail.com.com>
2023-10-16 20:05:28 +0300

    second commit

--- shonscript.js
+++ shonscript.js
@@ -1,22 +1,72 @@
+let tasks = [];
 
-document.getElementById("addTask").addEventListener("click", function () {
-    const taskInput = document.getElementById("task");
+
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
     const taskText = taskInput.value.trim();
 
-    if (taskText !== "") {
-        const tasksList = document.getElementById("tasks");
-        const taskItem = document.createElement("li");
-        taskItem.innerHTML = `
-            ${taskText}
-            <button class="delete">Delete</button>
-        `;
-        tasksList.appendChild(taskItem);
-
-        taskInput.value = ""; 
-
-    
-        taskItem.querySelector(".delete").addEventListener("click", function () {
-            taskItem.remove();
-        });
+    if (taskText) {
+        tasks.push({ text: taskText, completed: false });
+        taskInput.value = "";
+        displayTasks();
     }
-});
+}
+
+
+function displayTasks() {
+    const tasksContainer = document.getElementById("tasks");
+    tasksContainer.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const taskElement = document.createElement("div");
+        taskElement.className = "task";
+        if (task.completed) {
+            taskElement.classList.add("completed");
+        }
+
+        const textElement = document.createElement("span");
+        textElement.textContent = task.text;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.onclick = () => deleteTask(index);
+
+        const editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.onclick = () => editTask(index);
+
+        taskElement.appendChild(textElement);
+        taskElement.appendChild(deleteButton);
+        taskElement.appendChild(editButton);
+        tasksContainer.appendChild(taskElement);
+    });
+}
+
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    displayTasks();
+}
+
+
+function editTask(index) {
+    const newText = prompt("Edit task:", tasks[index].text);
+    if (newText !== null) {
+        tasks[index].text = newText;
+        displayTasks();
+    }
+}
+
+
+function markAllAsComplete() {
+    tasks.forEach((task) => (task.completed = true));
+    displayTasks();
+}
+
+
+function deleteAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
+displayTasks();
